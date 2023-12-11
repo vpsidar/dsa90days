d44d9e485a30b801819425dce8bbae87f2fb877c
Vijay Prakash Sidar <vpmsidar@gmail.com>
2023-12-11 22:37:32 +0530

    optimise solutions

--- day2/index.js
+++ day2/index.js
@@ -62,4 +62,23 @@ const romanToInt = function(s) {
     return value;
 };
 
+const romanToIntOptimise = function(s) {
+    let value = 0;
+    const map = new Map();
+    map.set('I',1);
+    map.set('V',5);
+    map.set('X',10);
+    map.set('L',50);
+    map.set('C',100);
+    map.set('D',500);
+    map.set('M',1000)
+    for(let i= 0; i<s.length;i++){
+        if(map.get(s[i]) < map.get(s[i+1])){
+            value = value-map.get(s[i]);
+        } else {
+            value = value + map.get(s[i]);
+        }
+    }
+    return value;
+};
 console.log(romanToInt('III'));
