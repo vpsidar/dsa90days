a0d98a09bb9a20c439e52ae9bf048bed3452b58e
Vijay Prakash Sidar <vpmsidar@gmail.com>
2023-12-16 19:37:08 +0530

    longest common prefix optimal solution

--- day3/index.js
+++ day3/index.js
@@ -2,3 +2,14 @@
 
 // How to solve this question
 //what are the things we can do 
+var longestCommonPrefix = function(strs) {
+    strs.sort();
+    let ans = "";
+    for(let i=0; i< Math.min(strs[0].length,strs[strs.length-1].length); i++){
+        if(strs[0][i] != strs[strs.length-1][i]){
+            return ans;
+        }
+        ans = ans+strs[0][i];
+    }
+    return ans;
+};
